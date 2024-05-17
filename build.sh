8f9c8a559b62a005e5f23fcf8bd0ebc9772a1af7
Denis Chaimow <dchaimow@users.noreply.github.com>
2024-05-17 16:03:57 +0200

    remove setting of tmp in build script

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 shopt -s extglob
 
-export APPTAINER_TMPDIR=/tmp
-export APPTAINER_CACHEDIR=/tmp
-
 deffile=${1-gfae.def}
 container_fname=$(basename $deffile .def).sif
-
 
 # BUILD CONTAINER
 tmpdir=$(mktemp -d)
